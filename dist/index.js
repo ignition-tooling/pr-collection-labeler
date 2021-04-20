5d64a965439a56daa90e1e47c358418906601632
Louise Poubel <louise@openrobotics.org>
2021-04-20 10:49:44 -0700

    Blueprint EOL (#13)
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- dist/index.js
+++ dist/index.js
@@ -1043,7 +1043,6 @@ async function run() {
     let labels = [];
 
     const collections = [
-      {name: 'blueprint', label: '📜 blueprint'},
       {name: 'citadel', label: '🏰 citadel'},
       {name: 'dome', label: '🔮 dome'},
       {name: 'edifice', label: '🏢 edifice'},
